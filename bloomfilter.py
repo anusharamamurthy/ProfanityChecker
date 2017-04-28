912d22ad59c260b7832be44c1693783d368b07cb
anuramam <anuramam@iu.edu>
2017-04-28 18:05:52 -0400

    #commit2

--- bloomfilter.py
+++ bloomfilter.py
@@ -28,39 +28,13 @@ class BloomFilter:
         :param word: Insert words into the bloom filter
         :return None: 
         """
-        # hashX0 = self.getBaseHash(word)
-        #
-        # # double hashing
-        # hashX1 = hashX0 + 2 * hashX0
-        # hashX2 = hashX0 + hashX1 * 3
-        # hashX3 = hashX0 + hashX1 + 4 * hashX2
-        # hashX4 = hashX0 + hashX1 + hashX2 + hashX3 * 5
-        # hashX5 = hashX0 + hashX1 + hashX2 + hashX3 + hashX4 * 6
-        # hashX6 = hashX0 + hashX1 + hashX2 + hashX3 + hashX4 + hashX5 * 7
 
         hashValues = self.generateHashes(word)
 
         for curHash in hashValues:
             self.bitArray |= 1 << curHash
 
-
-            # self.bitArray |= 1 << hashX0
-            # self.bitArray |= 1 << hashX1
-            # self.bitArray |= 1 << hashX2
-            # self.bitArray |= 1 << hashX3
-            # self.bitArray |= 1 << hashX4
-            # self.bitArray |= 1 << hashX5
-            # self.bitArray |= 1 << hashX6
-
     def isMember(self, lookup):
-
-        # hashX0 = self.getBaseHash(lookup)
-        # hashX1 = hashX0 + (2 * hashX0)
-        # hashX2 = hashX0 + hashX1 * 3
-        # hashX3 = hashX0 + hashX1 + 4 * hashX2
-        # hashX4 = hashX0 + hashX1 + hashX2 + hashX3 * 5
-        # hashX5 = hashX0 + hashX1 + hashX2 + hashX3 + hashX4 * 6
-        # hashX6 = hashX0 + hashX1 + hashX2 + hashX3 + hashX4 + hashX5 * 7
 
         hashValues = self.generateHashes(lookup)
 
@@ -68,13 +42,6 @@ class BloomFilter:
             if (self.bitArray >> hashVal) & 1 != 1:
                 return False
         return True
-
-        # if (self.bitArray >> hashX0) & 1 == 1 and (self.bitArray >> hashX1) & 1 == 1 \
-        #             (self.bitArray >> hashX2) & 1 == 1 and (self.bitArray >> hashX3) & 1 == 1 \
-        #             (self.bitArray >> hashX4) & 1 == 1 and (self.bitArray >> hashX5) & 1 == 1 \
-        #             (self.bitArray >> hashX6) & 1 == 1:
-        #     return True
-        # return False
 
     def __str__(self):
         return bin(self.bitArray)
@@ -85,7 +52,7 @@ class BloomFilter:
 
         hashes.append(self.getBaseHash(word))
 
-        for j in range(1,self.hashCount):
+        for j in range(1, self.hashCount):
             newHash = (hashes[j - 1] + (i * hashes[(0 if (j - 2) < 0 else (j - 2))])) % self.length
             i += 1
             hashes.append(newHash)
--- profanityChecker.py
+++ profanityChecker.py
@@ -1,34 +1,62 @@
 import bloomfilter as bloom
 import math
-import unittest
 import pickle
+import string
 
-class Test(unittest.TestCase):
 
-    @classmethod
-    def getWords(self,fname):
+# Stop words to remove during processing the documents.
+# Some are taken from the internet and modified to suit our model.
+# https://github.com/Alir3z4/python-stop-words
+
+stopWords = ['a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an', 'and', 'any', 'are', 'arent', 'as',
+            'at', 'because', 'be', 'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by', 'cant',
+            'cannot',
+            'couldnt', 'did', 'didnt', 'do', 'does', 'doesnt', 'doing', 'dont', 'down', 'during', 'each', 'few', 'for',
+            'from',
+            'further', 'had', 'hadnt', 'has', 'hasnt', 'have', 'havent', 'having', 'he', 'hed', 'hell', 'hes', 'her',
+            'hers', 'here',
+            'heres', 'herself', 'him', 'himself', 'his', 'how', 'hows', 'i', 'id', 'ill', 'im', 'ive', 'if', 'in',
+            'into', 'is', 'isnt',
+            'it', 'its', 'itself', 'lets', 'more', 'me', 'most', 'mustnt', 'my', 'myself', 'no', 'nor', 'not', 'of',
+            'off', 'on', 'once',
+            'only', 'or', 'other', 'ought', 'our', 'ours', 'ourselves', 'out', 'over', 'own', 'same', 'shant', 'she',
+            'shell', 'shed', 'shes',
+            'should', 'shouldnt', 'so', 'some', 'such', 'than', 'that', 'thats', 'the', 'their', 'theirs', 'them',
+            'themselves', 'then',
+            'there', 'theres', 'they', 'theyd', 'theyll', 'theyre', 'theyve', 'this', 'those', 'though', 'through',
+            'to', 'too', 'under',
+            'until', 'up', 'very', 'was', 'wasnt', 'we', 'wed', 'well', 'were', 'weve', 'werent', 'what', 'whats',
+            'when', 'whens', 'where',
+            'wheres', 'which', 'while', 'who', 'whos', 'whom', 'why', 'whys', 'with', 'wont', 'would', 'wouldnt',
+            'you', 'youd', 'youll',
+            'youre', 'youve', 'your', 'yours', 'yourself', 'yourselves', 'font', 'html', 'table', 'br', 'will',
+            'article', 'says', 'can', 'one', 'use', 'writes']
+
+class ProfanityChecker():
+
+    def getWords(self, fname):
 
         with open(fname, 'r') as fp:
             line = fp.readlines()
         fp.close()
         return line
 
-    def getText(self,fname):
+    def getText(self, fname):
 
         with open(fname, 'r') as fp:
             line = fp.readlines()
         fp.close()
         return line
 
-    def pickleFilter(self,instance):
+    def pickleFilter(self, instance,fname):
 
-        pkl = open("bloomFilter.pkl",'wb')
-        pickle.dump(instance,pkl)
+        pkl = open(fname, 'wb')
+        pickle.dump(instance, pkl)
         pkl.close()
 
-    def unPickleFilter(self):
+    def unPickleFilter(self,fname):
 
-        pkl = open("bloomFilter.pkl",'rb')
+        pkl = open(fname, 'rb')
         self.myFilter = pickle.load(pkl)
         pkl.close()
 
@@ -36,41 +64,58 @@ class Test(unittest.TestCase):
 
         """set up bloom filter to insert profane words"""
         words = self.getWords("profanity_en.txt")
-        self.myFilter = bloom.BloomFilter(4173)
+        self.myFilter = bloom.BloomFilter(2000, 3)
         for word in words:
             self.myFilter.insert(word.strip().lower())
 
+    def sanitize_content(self,content):
+
+        content = content.lower()
+        rmv = string.punctuation + '\t\n'
+        translator = str.maketrans('', '', rmv)
+        content = content.translate(translator)
+        # content = content.translate(string.digits)
+        sanitized_content = [word for word in content.split() if word not in stopWords]
+        return sanitized_content
+
+
+
     def testMembership(self):
+
         """
         check for the number of false positives
         """
         # self.setUp()
-        # self.pickleFilter(self.myFilter)
-        self.unPickleFilter()
-        text = self.getText("/Users/aramamurthy/Downloads/Github/ProfanityChecker/Brennan-Greenstadt-Corpus/a/a_01.txt")
-        falsePos = []
-        for words in text:
-            for word in words.split():
-                if self.myFilter.isMember(word.lower()):
-                    falsePos.append(word)
+        # self.pickleFilter(self.myFilter,"BloomFilter")
+        self.unPickleFilter("BloomFilter")
+
+        # flag text for review
+        flagged = []
+        text = "This is sweet. They should shut up."
 
 
-        # self.calOptArrayLen()
+        parsedText = self.sanitize_content(text)
 
+        for word in parsedText:
+            if self.myFilter.isMember(word):
+                flagged.append(word)
+
+        print("These words are flagged:")
+        print(flagged)
 
     def calOptArrayLen(self):
 
         setLen = eval(input("Please enter the size of your set:"))
         errRate = float(input("Please enter the acceptable false positive rate[0.0 - 1.0]:"))
 
-        bArrLen = (setLen * math.log(errRate,math.e)) / (math.log(2,math.e)**2)
+        bArrLen = (setLen * math.log(errRate, math.e)) / (math.log(2, math.e) ** 2)
 
-        kfnc = (bArrLen * math.log(2,math.e)) / setLen
+        kfnc = (bArrLen * math.log(2, math.e)) / setLen
 
-        print("Optimal Bit Vector Length is:",abs(math.floor(bArrLen)))
-        print("Optimal number of Hash functions is:",abs(math.floor(kfnc)))
+        print("Optimal Bit Vector Length is:", abs(math.floor(bArrLen)))
+        print("Optimal number of Hash functions is:", abs(math.floor(kfnc)))
 
 
 if __name__ == '__main__':
-    unittest.main()
-
+    myCheck = ProfanityChecker()
+    myCheck.testMembership()
